2f081152669acdecfa647d662004fb8bfe76da61
Julian Ramirez Robayo <87994643+JRobayo99@users.noreply.github.com>
2026-02-09 14:32:06 -0500

    Delete pth310 directory

--- pth310/pruebacam.py
+++
@@ -1,8 +0,0 @@
-import cv2
-cap = cv2.VideoCapture(0)
-print("Camara abierta:", cap.isOpened())
-
-
-print("WIDTH:", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-print("HEIGHT:", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-cap.release()
